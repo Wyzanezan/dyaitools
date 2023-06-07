f79ce3d44b3e7ca586e10cbd0ff37e4b6a824437
wyzane63151207 <wyzane1207@gmail.com>
2023-06-07 17:20:38 +0800

    aitools开发

--- urls.py
+++ urls.py
@@ -2,9 +2,10 @@
 
 
 from AnalysisHandler import ProjectAnalysisHandler
+from AnalysisWebsocket import ProjectAnalysisWebsocket
 
 
 urls = [
     (r'/vis/ai/analysis/', ProjectAnalysisHandler),
+    (r'/ai/analysis/websocket/', ProjectAnalysisWebsocket),
 ]
-
