71453ae366e6ac25625a12fb21767c0241eb48b8
wyzane63151207 <wyzane1207@gmail.com>
2023-06-08 15:31:35 +0800

    aitools开发

--- AnalysisHandler.py
+++ AnalysisHandler.py
@@ -5,7 +5,7 @@ import time
 import tornado.web
 
 from config import logger, white_list
-from utils_02 import Conversation
+from utils_02 import Conversation, ConversationStream
 
 
 class ProjectAnalysisHandler(tornado.web.RequestHandler):
@@ -28,10 +28,19 @@ class ProjectAnalysisHandler(tornado.web.RequestHandler):
         logger.info(f"request prompt info is: {prompt}, message is: {message}")
 
         # 调用openai接口
-        con = Conversation(prompt)
+        # con = Conversation(prompt)
+        con = ConversationStream(prompt)
         response = con.ask_question(message)
         resp_code, answer = response[0], response[1]
         logger.info(f"answer info is: {answer}")
 
-        logger.info(f"response time: {time.time() - start_time}")
-        self.write({"code": resp_code, "answer": answer})
+        if resp_code != 1000:
+            self.write({"code": resp_code, "answer": answer})
+
+        self.set_header("Content-type", "application/stream")
+        for resp in response:
+            content = resp["choice"][0]["delta"]["content"]
+            logger.info(f"content: {content}")
+            self.write({"code": resp_code, "answer": content})
+
+
--- utils_02.py
+++ utils_02.py
@@ -78,3 +78,63 @@ class Conversation(object):
             if finish_reason == "stop" and message:
                 resp_data["answer"] = message.get("content")
         return 1000, resp_data
+
+
+class ConversationStream(object):
+
+    def __init__(self, prompt, model="gpt-3.5-turbo-0301"):
+        self.prompt = prompt
+        self.model = model
+        self.data_length = 2000
+        self.messages = []
+        self.messages.append({"role": 'system', "content": self.prompt})
+
+    def get_answer(self, message):
+        self.messages.append({"role": 'user', "content": message})
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=self.messages,
+            max_tokens=2000,
+            stream=True
+        )
+        return response
+
+    def check_data_length(self):
+        try:
+            encoding = tiktoken.encoding_for_model(self.model)
+        except KeyError:
+            logger.info("Warning: model not found. Using cl100k_base encoding.")
+            encoding = tiktoken.get_encoding("cl100k_base")
+
+        ret = encoding.encode(self.prompt)
+        return len(ret)
+
+    def ask_question(self, message):
+        request_tokens = self.check_data_length()
+        logger.info(f"request tokens num: {request_tokens}")
+
+        if request_tokens > self.data_length:
+            return 24003, "data too long"
+
+        try:
+            response = self.get_answer(message)
+        except openai.error.RateLimitError:
+            logger.info("request open ai rate limit, retry again ... ")
+
+            try:
+                response = self.get_answer(message)
+            except Exception as e:
+                logger.info(f"request open ai exception: {traceback.format_exc()}")
+                response = None
+        except openai.error.InvalidRequestError:
+            logger.info(f"request open ai exception: {traceback.format_exc()}")
+            return 24003, "data too long"
+        except Exception as e:
+            logger.info(f"request open ai exception: {traceback.format_exc()}")
+            response = None
+
+        logger.info(f"openai response info: {response}")
+        if not response:
+            return 24001, "system error"
+
+        return 1000, response
